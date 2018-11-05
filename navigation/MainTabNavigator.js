c65cda0aace5821c26230cba774b71460d8bee38
iagrigor <iagrigor@cisco.com>
2018-11-05 23:28:30 +0200

    oki doki

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,8 +4,6 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -28,6 +26,4 @@ HomeStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
-  SettingsStack,
 });
